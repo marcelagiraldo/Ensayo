543e015ec5acbfbfd3a60f0815b87c0321fb02fc
marcelagiraldo <leidymarcelaalzategiraldo@gmail.com>
2022-04-07 16:31:05 -0500

    Primer intento

---
+++ src/ensayogithub/EnsayoGitHub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ensayogithub;
+
+/**
+ *
+ * @author Marcela Alzate
+ */
+public class EnsayoGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 1;
+        int b = 2;
+        
+        System.out.println("Suma "+(a+b));
+    }
+    
+}
